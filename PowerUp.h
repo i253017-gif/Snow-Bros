c3dfa12dc28ea734e88d7d3856487634bac3cb7e
Ishmal Najeeb (RollNo: 2125) <i252125@isb.nu.edu.pk>
2026-04-20 19:31:08 +0500

    CORRECT VERSION

--- PowerUp.h
+++ PowerUp.h
@@ -1,1 +1,39 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "hit_box.h" 
+
+class player;
+
+class PowerUp {
+protected:
+    //positions
+    float   pos_x,  pos_y;    
+    //duration the effect is gonna stau for
+    float   duration;  
+    hit_box hitbox;
+
+
+public:
+
+    //constructor
+    PowerUp(float x, float y, float dur);
+    ~PowerUp();
+    //pure virtual funcs which make the class abstract
+
+    virtual void apply(player& p) = 0; //apply the effect to the player
+    virtual void draw(sf::RenderWindow& window) = 0;//drawing powerups
+    virtual void expire(player& p) = 0;//the duration of powerup ends so the effect needs to be removed
+
+    //GETTERS AND SETTERS
+
+   
+   
+    void set_pos_x(float x);
+    void set_pos_y(float y);
+    void set_duration(float dur);
+  
+    float   get_pos_x();
+    float   get_pos_y();
+    float   get_duration();
+    hit_box get_hit_box();
+};
