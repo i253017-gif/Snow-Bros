bfacfc9defb7048bbb2dea98a18a7dcf2744dbca
Ishmal Najeeb (RollNo: 2125) <i252125@isb.nu.edu.pk>
2026-04-20 17:33:45 +0500

    Add files via upload

---
+++ PowerUp.cpp
@@ -0,0 +1,38 @@
+#include "PowerUp.h"
+
+/////PURE VIRTUAL FUNCTIONS WONT BE DEFINED IN THIS CLASS THEY WILL BE DEFINED IN THE CHILD CLASSES
+
+//constructo initialization
+
+PowerUp::PowerUp(float x, float y, float dur) {
+	pos_x = x;
+	pos_x = y;
+	duration = dur;
+}
+
+//SETTERS AND GETTERS
+
+
+float PowerUp::get_pos_x() {
+	return pos_x; 
+}
+float PowerUp::get_pos_y() {
+	return pos_y; 
+}
+float PowerUp::get_duration() {
+	return duration;
+}
+hit_box PowerUp::get_hit_box() { 
+	return hitbox; 
+}
+
+void PowerUp::set_pos_x(float x) { 
+	pos_x = x;
+}
+void PowerUp::set_pos_y(float y) {
+	pos_y = y;
+}
+void PowerUp::set_duration(float dur) {
+		duration = dur;
+	}
+
