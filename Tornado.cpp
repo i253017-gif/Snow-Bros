088532c2de64fca5fd53fc13133e38449498aed9
Ishmal Najeeb (RollNo: 2125) <i252125@isb.nu.edu.pk>
2026-04-21 20:02:11 +0500

    Add files via upload

---
+++ Tornado.cpp
@@ -0,0 +1,35 @@
+#include"Tornado.h"
+Tornado::Tornado(float speed_mult, int extra_hits):FlyingFooga(speed_mult, extra_hits) {
+    if (tex.loadFromFile("assets/FlyingFooga_red.png")) {
+        visual.setTexture(tex);
+    }
+  
+    this->knife_timer = 2.5f;
+}
+
+void Tornado::draw(sf::RenderWindow& window) {
+
+    visual.setPosition(pos_x, pos_y);
+    window.draw(visual);
+
+    
+
+}
+
+void Tornado::move(float delta_time) {}
+void Tornado::update(float time) {}
+
+Tornado::~Tornado() {}
+
+
+
+//GETTERS AND SETTERS
+
+void Tornado::set_knife_timer(float time) {
+    knife_timer = time;
+
+}
+
+float Tornado::get_knife_timer() {
+    return knife_timer;
+}
