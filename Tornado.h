e83a192abe64eddfd37aec9f44ae93f335e344e6
Ishmal Najeeb (RollNo: 2125) <i252125@isb.nu.edu.pk>
2026-04-21 20:01:43 +0500

    Add files via upload

---
+++ Tornado.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "FlyingFooga.h"
+class Tornado :public FlyingFooga{
+protected:
+	float knife_timer; //it keeps throwing knives so its a timer for hoq long one knife is there before the other one is thrown
+public:
+	Tornado(float speed_mult, int extra_hits);
+
+
+	void draw(sf::RenderWindow& window) override;
+	void move(float delta_time) override;
+	void update(float time) override;
+	
+
+	//getter ND setters
+	float get_knife_timer();
+	void set_knife_timer(float time);
+
+	~Tornado();
+};
