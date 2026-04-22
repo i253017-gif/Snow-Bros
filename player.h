6ac58159e68ecba4ed0fe6882c0e8eaf709a3965
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-04-22 21:02:20 +0500

    Add Player class definition with basic attributes

---
+++ player.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+#include"inputmanager.h"
+
+class Player
+{
+private:
+	sf::RectangleShape shape;      //player shape and like the hitbox shape as well
+	sf::Sprite sprite;             
+	sf::Texture texture;
+	float velocityX;			   //per frame movement rate
+	float velocityY;
+	float gravity;
+	float Jumpstrength;
+	bool isOnGround; 			   //no double jump 
+public:
+	Player();
+
+	void handleInput(inputManager& input);
+	void update();
+	void render(sf::RenderWindow& window);
+	void applyGravity();
+
+	sf::FloatRect getBounds(); //hitbox
+};
