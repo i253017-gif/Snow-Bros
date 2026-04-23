ffc70537ade7eec3146d0e7ececa93be5a1aa7a4
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-04-23 11:51:22 +0500

    Add directional key press detection methods

--- inputmanaager.h
+++ inputmanaager.h
@@ -6,12 +6,16 @@ class inputManager
 private:
 	bool enter;
 	bool escape;
+	bool left;
+	bool right;
+	bool up;
 
 public:
 	bool isEnterPressed();
 	bool isEscapePressed();
+	bool isLeftPressed();
+	bool isRightPressed();
+	bool isUpPressed();
 	void update();
 
-
-	//will add motion detection for player later
 };
