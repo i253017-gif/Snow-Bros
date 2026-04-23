283dd38e8ca8763db2a00fe752a8eb9930f3e864
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-04-23 16:57:05 +0500

    Add player member and font/text variables to playState

--- playstate.h
+++ playstate.h
@@ -2,11 +2,17 @@
 #include<SFML/Graphics.hpp>
 #include"gamestate.h"
 #include"statemachinee.h"
+#include"player.h"
 
 class playState : public gameState
 {
 private:
 	stateMachine* machine;
+
+	//=====================
+	sf::Font font;
+	sf::Text text;
+	Player player;
 
 public:
 	playState(stateMachine* m);
@@ -14,6 +20,7 @@ public:
 	void update();
 	void render(sf::RenderWindow& window);
 	~playState();
+
 
 	//actual game logic will come here
 };
