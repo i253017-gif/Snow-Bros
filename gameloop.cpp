a6e5a910ad534b269318495a7c6a487115dbd23c
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-04-23 11:54:53 +0500

    Refactor gameloop with player input handling

--- gameloop.cpp
+++ gameloop.cpp
@@ -3,16 +3,16 @@
 #include"menustate.h"
 #include"statemachinee.h"
 #include<iostream>
+#include"player.h"
 //include menu state
 
 Gameloop::Gameloop() : window(sf::VideoMode(700, 700), "Snow Bros")
 {                       //so window is not declared locally but is a class member
     machine.changeState(new menuState(&machine));
 }
-
 void Gameloop::run() {
     while (window.isOpen()) {
-			std::cout << "For testing! Entered gameloop run function\n";
+		std::cout << "For testing! Entered gameloop run function\n";
         // 🔹 1. Handle window events
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -23,11 +23,17 @@ void Gameloop::run() {
         // 🔹 2. Update input
         input.update();
 
-        // 🔹 3. Let current state handle input + update
+
+        // 3. Player
+        player.handleInput(input);
+        player.update();
+
+
+        // 🔹 4. Let current state handle input + update
         machine.handleInput(input);
         machine.update();
 
-        // 🔹 4. Draw current state
+        // 🔹 5. Draw current state
         window.clear();
         machine.render(window);
         window.display();
