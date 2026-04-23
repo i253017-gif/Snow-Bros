ad291ed91cd0d2dd24c95659fda1ddf92709198e
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-04-23 11:52:15 +0500

    Add key press detection for left, right, and up for player movement

--- inputmanager.cpp
+++ inputmanager.cpp
@@ -9,11 +9,24 @@ bool inputManager::isEscapePressed()
 {
 	return escape;
 }
+bool inputManager::isLeftPressed()
+{
+	return left;
+}
+bool inputManager::isRightPressed()
+{
+	return right;
+}
+bool inputManager::isUpPressed()
+{
+	return up;
+}
 void inputManager::update()
 {
 	enter = sf::Keyboard::isKeyPressed(sf::Keyboard::Enter); //sfml handling ugh
 	escape = sf::Keyboard::isKeyPressed(sf::Keyboard::Escape);
+	left = sf::Keyboard::isKeyPressed(sf::Keyboard::A);
+	right = sf::Keyboard::isKeyPressed(sf::Keyboard::D);
+	up = sf::Keyboard::isKeyPressed(sf::Keyboard::W);
 
-
-	//add motion later
 }
