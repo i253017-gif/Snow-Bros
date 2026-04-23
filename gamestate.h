829a753aafb5d9f861ef3db7db2cf7a940cc1880
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-04-23 16:54:39 +0500

    Rename update method to handleInput in gameState

--- gamestate.h
+++ gamestate.h
@@ -4,10 +4,11 @@
 class gameState
 {
 public:
-	virtual void update(inputManager& input) = 0;
+	virtual void handleInput(inputManager& input) = 0;
 	virtual void update() = 0;
 	virtual void render(sf::RenderWindow& window) = 0;
 
 	virtual ~gameState();
 };
 //just a base class for states so does not need a cpp
+//all states inherit from this
