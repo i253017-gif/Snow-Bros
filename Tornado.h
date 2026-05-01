67207fba6fddb04eeb896f9ef86117d8d2c9d2ba
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-05-01 08:09:43 +0500

    Add Tornado class with knife throwing functionality

---
+++ Tornado.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "FlyingFooga.h"
+#include"knife.h"
+
+class Tornado :public FlyingFooga {
+protected:
+	float knife_timer;  //counts down to next knife throw
+	float teleportTime;
+	float currentTime;
+	knife* knives[200]; 
+	int knifeNum;
+	float playerX, playerY;
+
+public:
+	Tornado(float x, float y, float speed_mult, int extra_hits);
+
+	void draw(sf::RenderWindow& window) override;
+	void move(float delta_time) override;
+	void update(float time) override;
+
+	void set_player_position(float x, float y);  
+
+	float get_knife_timer();
+	void set_knife_timer(float time);
+
+	void Teleport();
+	~Tornado();
+};
