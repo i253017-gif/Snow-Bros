6163c87c5855f2550e45c418445580b68ccd4aec
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-05-02 16:17:28 +0500

    Refactor Gamakichi class and implement movement logic

--- gamachiki.cpp
+++ gamachiki.cpp
@@ -1,47 +1,130 @@
-#pragma once
-#include"Boss.h"
-#include"artillery_rocket.h"
+#include"Gamachiki.h"
 
-struct RocketData {
-	float x, y;
-	float vx, vy;
-	float blast_radius;
-};
+Gamakichi::Gamakichi(float x, float y, int hp) :Boss(x, y, hp, hp) {
+	shape.setSize(sf::Vector2f(50, 50));
+	shape.setFillColor(sf::Color::Red);
+	shape.setPosition(x, y);
+	if (text.loadFromFile("assets/Gamakichi.png"))
+	{
+		visual.setTexture(text);
+		visual.setPosition(shape.getPosition());
+	}
 
-class Gamakichi :public Boss {
-private:
-	float rocket_timer;
-	float rocket_spawn_interval;
-	int gem_reward;
+	pos_x = x;
+	pos_y = y;
 
-	// Movement
-	float platform_y_position;
-	float platform_move_timer;
-	float platform_move_speed;
-	float platform_min_y;
-	float platform_max_y;
-	bool moving_up;
+	rocket_timer = 0.0f;
+	rocket_spawn_interval = 3.0f;
+	gem_reward = 1000;
 
-public:
-	Gamakichi(float x, float y, int hp);
-	~Gamakichi();
+	// Platform movement
+	platform_y_position = y;
+	platform_move_timer = 0.0f;
+	platform_move_speed = 0.5f;
+	platform_min_y = 100.0f;
+	platform_max_y = 500.0f;
+	moving_up = false;
+}
 
-	void draw(sf::RenderWindow& window) override;
-	void move(float delta_time) override;
-	void update(float delta_time) override;
-	sf::FloatRect getBounds() override;
-	std::string get_type() const override;
-	void update_attack_phase();
+void Gamakichi::draw(sf::RenderWindow& window) {
+	//visual.setPosition(pos_x, pos_y);
+	//window.draw(visual);
+	window.draw(shape);
+	show_health_bar(window);
+}
 
-	RocketData* get_rockets_to_spawn(int& count);
+void Gamakichi::move(float delta_time) {
+	if (moving_up) {
+		platform_y_position -= platform_move_speed;
+		if (platform_y_position <= platform_min_y) {
+			moving_up = false;
+		}
+	}
+	else {
+		platform_y_position += platform_move_speed;
+		if (platform_y_position >= platform_max_y) {
+			moving_up = true;
+		}
+	}
 
-	// Getters and setters
-	float get_rocket_timer();
-	int get_gem_reward();
-	void set_rocket_timer(float time);
-	void set_gem_reward(int reward);
-	float get_rocket_spawn_interval();
+	pos_y = platform_y_position;
+}
 
-	float get_platform_y();
-	void set_platform_y(float y);
-};
+void Gamakichi::update(float delta_time) {
+	update_attack_phase();
+	move(delta_time);
+
+	rocket_timer += delta_time;
+}
+
+void Gamakichi::update_attack_phase() {
+	float hp_percentage = (float)hp / (float)boss_hp;
+
+	if (hp_percentage > 0.65f) {
+		attack_phase = 1;
+		rocket_spawn_interval = 3.0f;
+	}
+	else if (hp_percentage > 0.35f) {
+		attack_phase = 2;
+		rocket_spawn_interval = 2.0f;
+	}
+	else {
+		attack_phase = 3;
+		rocket_spawn_interval = 1.0f;
+	}
+}
+
+RocketData* Gamakichi::get_rockets_to_spawn(int& count) {
+	static RocketData rockets[4];
+	count = 4;
+
+	float spawn_x = pos_x + visual.getLocalBounds().width / 2;
+	float spawn_y = pos_y;
+
+	rockets[0] = { spawn_x, spawn_y, 0.0f, -5.0f, 50.0f };
+	rockets[1] = { spawn_x, spawn_y, -2.0f, -4.5f, 50.0f };
+	rockets[2] = { spawn_x, spawn_y, 2.0f, -4.5f, 50.0f };
+	rockets[3] = { spawn_x, spawn_y, 0.0f, -5.0f, 50.0f };
+
+	return rockets;
+}
+
+float Gamakichi::get_rocket_timer() {
+	return rocket_timer;
+}
+
+int Gamakichi::get_gem_reward() {
+	return gem_reward;
+}
+
+void Gamakichi::set_rocket_timer(float time) {
+	rocket_timer = time;
+}
+
+void Gamakichi::set_gem_reward(int reward) {
+	gem_reward = reward;
+}
+
+float Gamakichi::get_platform_y() {
+	return platform_y_position;
+}
+
+void Gamakichi::set_platform_y(float y) {
+	platform_y_position = y;
+	pos_y = platform_y_position;
+}
+float Gamakichi::get_rocket_spawn_interval() {
+	return rocket_spawn_interval;
+}
+
+Gamakichi::~Gamakichi() {}
+
+sf::FloatRect Gamakichi::getBounds()
+{
+	return shape.getGlobalBounds();
+}
+
+std::string Gamakichi::get_type() const
+{
+	return "Gamakichi";
+}
