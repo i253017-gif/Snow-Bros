8bdd06fc622ebb04c675f5a4f4b3595e84e59b8b
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-05-02 10:29:19 +0500

    Extend Player class with game mechanics variables
    Added member variables for lives, score, gems, and power-ups.

--- player.h
+++ player.h
@@ -15,6 +15,17 @@ private:
 	bool isOnGround; 	           //no double jump
 	int facingDirection; 
 	bool wasSpacePressed;
+
+	int lives;
+	int score;
+	int gem;
+	float invincibilityTime;
+
+	bool snowball_power_active;
+	float snowball_distance;
+	float original_snowball_distance;
+	bool balloon_mode_active;
+
 public:
 	Player();
 
@@ -31,4 +42,23 @@ public:
 	void setOnGround(bool onGround);
 
 	sf::FloatRect getBounds(); //hitbox
+
+
+	int getLives();
+	void loseLive();
+	void resetToStart();    //after death
+	bool isInvincible();
+	int getScore();
+	void addScore(int points);
+	void addGems(int amount);
+	int getGems();
+
+
+	void set_speed(float speed);
+	float get_speed();
+	void set_snowball_power(bool active);
+	void set_snowball_distance(float distance);
+	float get_snowball_distance();
+	void set_balloon_mode(bool active);
+	
 };
