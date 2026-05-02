e58523a1bc69c77dad26d02dae8d5ff2276a330c
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-05-02 10:09:15 +0500

    Add new attributes and methods to playState class

--- playstate.h
+++ playstate.h
@@ -7,6 +7,10 @@
 #include"platform.h"
 #include"Botom.h"
 #include"Enemy.h"
+#include"Projectile.h"
+#include"artillery_rocket.h"
+#include"PowerUp.h"
+#include"PowerUp_funcs.h"
 
 class playState : public gameState
 {
@@ -22,12 +26,31 @@ private:
 	bool wasSpacePressed;  //so only one snowball per key press
 	Platform platform[50];
 	int Pnum;
-	Enemy* enemies[200];
+	Enemy* enemies[500];
 	int Enum;
+	artillery_rocket* projectile[500];  
+	int projectile_count;
 
+	PowerUp_funcs* powerups[50];
+	int powerup_count;
 
+	int lives;
+	int score;
 
+	int level;
 
+	
+	//================GEM RAIN 
+	static const int MAX_GEMS = 100;
+	float gemX[MAX_GEMS];
+	float gemY[MAX_GEMS];
+	float gemVelocity[MAX_GEMS];
+	bool gemCollected[MAX_GEMS];
+	int gemCount = 0;
+	float cashTimer = 0.0f;
+	bool isCashRain = false;
+	//LOAD HUD LATER
+	//
 public:
 	playState(stateMachine* m);
 	void handleInput(inputManager& input);
@@ -36,6 +59,19 @@ public:
 	~playState();
 	void loadLevel(int levelNum);
 
+	void spawnPowerUp(float x, float y);
+	void updatePowerUps(float delta_time);
+	void checkPowerUpCollisions();
+	void removePowerUp(int index);
 
-	//actual game logic will come here
+	int getScore();
+
+
+	int getLevel();
+	void setLevel(int lv);
+
+	void spawnGemRain(int amount, bool isCash);
+	void updateGems(float deltaTime);
+	void checkGemCollisions();
+	void renderGems(sf::RenderWindow& window);
 };
