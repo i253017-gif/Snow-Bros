fc40e0882f42218b4372f6867eae38fad7237d0f
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-05-02 19:14:35 +0500

    Add player 2 controls and number input flags

--- inputmanaager.h
+++ inputmanaager.h
@@ -11,6 +11,14 @@ private:
 	bool up;
 	bool space; //snowball
 
+	bool player2Left;
+	bool player2Right;
+	bool player2Up;
+
+	bool num1; //singleplayer
+	bool num2; //multiplayer
+
+
 public:
 	bool isEnterPressed();
 	bool isEscapePressed();
@@ -18,6 +26,14 @@ public:
 	bool isRightPressed();
 	bool isUpPressed();
 	bool isSpacePressed();
+
 	void update();
 
+
+	bool isPlayer2LeftPressed();
+	bool isPlayer2RightPressed();
+	bool isPlayer2UpPressed();
+
+	bool isNum1Pressed();
+	bool isNum2Pressed();
 };
