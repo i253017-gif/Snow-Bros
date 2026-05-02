aa7a508cfbad5e76829d98414434875e1ae26d31
Abiha Jibbran <i253017@isb.nu.edu.pk>
2026-05-02 19:15:05 +0500

    Add player 2 controls and number key checks

--- inputmanager.cpp
+++ inputmanager.cpp
@@ -33,5 +33,32 @@ void inputManager::update()
 	right = sf::Keyboard::isKeyPressed(sf::Keyboard::D);
 	up = sf::Keyboard::isKeyPressed(sf::Keyboard::W);
 	space = sf::Keyboard::isKeyPressed(sf::Keyboard::Space); //snowball
+	player2Left = sf::Keyboard::isKeyPressed(sf::Keyboard::Left);   //left arrow
+	player2Right = sf::Keyboard::isKeyPressed(sf::Keyboard::Right); 
+	player2Up = sf::Keyboard::isKeyPressed(sf::Keyboard::Up);  
+	num1 = sf::Keyboard::isKeyPressed(sf::Keyboard::Num1);  //singleplayer
+	num2 = sf::Keyboard::isKeyPressed(sf::Keyboard::Num2);
 
+}
+
+bool inputManager::isPlayer2LeftPressed()
+{
+	return player2Left;
+}
+bool inputManager::isPlayer2RightPressed()
+{
+	return player2Right;
+}
+bool inputManager::isPlayer2UpPressed()
+{
+	return player2Up;
+}
+
+bool inputManager::isNum1Pressed()
+{
+	return num1;
+}
+bool inputManager::isNum2Pressed()
+{
+	return num2;
 }
